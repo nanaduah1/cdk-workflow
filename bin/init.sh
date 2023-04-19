af89e85e4797e0f14726af97846624d14ca2cda1
Lawrence Duah <59175088+nanaduah1@users.noreply.github.com>
2023-04-19 16:20:05 -0400

    Extract reusable workflows

--- bin/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Find all folders containing pyproject.toml
-# then execute poetry install command
-dir=./src
-find "$dir" -name pyproject.toml -printf "%h\n" | sort -nr | while read -r i; do cd "$i" && poetry install; done
--- src/api1/tests/test_api1.py
+++
@@ -1,8 +0,0 @@
-from api1.handler import handler
-
-
-def test_api1():
-    tested = handler
-    expected = 200
-    response = tested(event={}, context={})
-    assert response["statusCode"] == expected
